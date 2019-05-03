c1599a1d6adf191e891f8fe3f558c7b84caba165
Gabriel Ernesto Manuel Martinez Escobar <gernestome@hotmail.com>
2019-05-02 20:37:36 -0500

    axios

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow
  */
 
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   View,
   Text,
@@ -17,33 +17,32 @@ import {
 } from 'react-native';
 import Header from 'components/Header';
 import RestaurantRow from 'components/RestaurantRow';
-
-const restaurants = [
-  {name: 'React Cafe', address: '123 Anywhere St'},
-  {name: 'Fancy Restaurant', address: '799 Main St'},
-  {name: 'Taco Place', address: '550 Maple Rd'},
-  {name: "Tony's Diner", address: '4101 College St'},
-  {name: 'Pasta Central', address: '706 Harper St'},
-  {name: 'Burger Builder', address: '4869 Hamilton Dr'},
-  {name: 'Pizza Express', address: '1049 Bird St'},
-  {name: 'Teriyaki To Go', address: '1885 Tea Berry Lane'},
-  {name: 'Maroon Deli', address: '1082 Stuart St'},
-  {name: 'Prime Bar and Grill', address: '1848 Fairfax Dr'},
-  {name: 'Dumpling House', address: '747 Kelly Dr'},
-  {name: 'Hot Chicken', address: '1816 Olive St'},
-  {name: "Luna's Tap Room", address: '3256 Spirit Dr'},
-  {name: 'Quick Sandwich Shop', address: '2587 Cherry Ridge Dr'},
-  {name: "Bobby's Burgers", address: '4152 Berkley St'},
-  {name: 'Turnpike Diner', address: '4571 Central Ave'},
-  {name: 'Bombay Express', address: '65 Queens Lane'},
-  {name: 'Coffee Central', address: '3228 Oakwood Circle'},
-  {name: "King's Garden", address: '2935 Victoria Ct'},
-  {name: 'Salads and More', address: '2454 Preston St'},
-];
+import axios from 'axios';
 
 type Props = {};
 const App = (props: Props) => {
     const [search, setSearch] = useState('');
+    const [restaurants, setRestaurants] = useState([]);
+
+    // componentDidMount / empty bracket sould do the same job
+    /* useEffect(() => {
+      fetch('http://localhost:3000/api/Restaurants/places')
+        .then(response => {
+          return response.json()
+        })
+        .then(result => {
+          setRestaurants(result)
+        })
+    }, []) */
+    useEffect(() => {
+      axios.get('http://localhost:3000/api/Restaurants/places')
+        .then(result => {
+          console.log('axios::get');
+          const { data } = result;
+          setRestaurants(data)
+        })
+    }, []);
+
     return (
       <View style={{flex: 1}}>
         <Header />
@@ -87,3 +86,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5F5F5'
   }
 })
+
+
+/* const restaurants = [
+  {name: 'React Cafe', address: '123 Anywhere St'},
+  {name: 'Fancy Restaurant', address: '799 Main St'},
+  {name: 'Taco Place', address: '550 Maple Rd'},
+  {name: "Tony's Diner", address: '4101 College St'},
+  {name: 'Pasta Central', address: '706 Harper St'},
+  {name: 'Burger Builder', address: '4869 Hamilton Dr'},
+  {name: 'Pizza Express', address: '1049 Bird St'},
+  {name: 'Teriyaki To Go', address: '1885 Tea Berry Lane'},
+  {name: 'Maroon Deli', address: '1082 Stuart St'},
+  {name: 'Prime Bar and Grill', address: '1848 Fairfax Dr'},
+  {name: 'Dumpling House', address: '747 Kelly Dr'},
+  {name: 'Hot Chicken', address: '1816 Olive St'},
+  {name: "Luna's Tap Room", address: '3256 Spirit Dr'},
+  {name: 'Quick Sandwich Shop', address: '2587 Cherry Ridge Dr'},
+  {name: "Bobby's Burgers", address: '4152 Berkley St'},
+  {name: 'Turnpike Diner', address: '4571 Central Ave'},
+  {name: 'Bombay Express', address: '65 Queens Lane'},
+  {name: 'Coffee Central', address: '3228 Oakwood Circle'},
+  {name: "King's Garden", address: '2935 Victoria Ct'},
+  {name: 'Salads and More', address: '2454 Preston St'},
+]; */
